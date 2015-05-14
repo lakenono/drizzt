1de7c412b8d21c7c16291d75ef0b92657666f261
lakenono <lakenono@126.com>
2015-05-14 15:42:57 +0800

    mapping代码完成..

--- src/test/java/httpclient/BaseMappingDemo.java
+++ src/test/java/httpclient/BaseMappingDemo.java
@@ -12,6 +12,7 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.junit.Test;
 
+// 原始未封装的mapping
 public class BaseMappingDemo
 {
 
--- src/test/java/httpclient/CookieMappingDemo.java
+++ src/test/java/httpclient/CookieMappingDemo.java
@@ -13,13 +13,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
 import org.apache.http.Header;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.config.RequestConfig.Builder;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
 import drizzt.domain.AdxUser;
 
+// mapping封装
 @Slf4j
 public class CookieMappingDemo
 {
@@ -29,7 +33,6 @@ public class CookieMappingDemo
 	}
 
 	private Map<String, String> mappingUrls = new HashMap<String, String>();
-	private CloseableHttpClient httpclient = HttpClients.createDefault();
 
 	public CookieMappingDemo()
 	{
@@ -40,14 +43,31 @@ public class CookieMappingDemo
 		this.mappingUrls.put("cm.allyes.com", "http://cm.allyes.com/pixel?allyes_dspid=174&allyes_cm&ext_data=");
 	}
 
-	public void run() throws SQLException, ClientProtocolException, IOException
+	public void run() throws SQLException
 	{
-		List<AdxUser> adxUsers = GlobalComponents.db.getRunner().query("select * from " + BaseBean.getTableName(AdxUser.class) + " where dspurl is null", new BeanListHandler<AdxUser>(AdxUser.class));
+		List<AdxUser> adxUsers = GlobalComponents.db.getRunner().query("select * from " + BaseBean.getTableName(AdxUser.class) + " where dspurl is null limit 100", new BeanListHandler<AdxUser>(AdxUser.class));
 
 		for (AdxUser adxUser : adxUsers)
 		{
-			AdxUser result = this.mapping(adxUser);
-			log.info(result.getDspurl());
+			AdxUser result = null;
+
+			try
+			{
+				result = this.mapping(adxUser);
+			}
+			catch (ClientProtocolException e)
+			{
+				log.error("{}", e);
+			}
+			catch (IOException e)
+			{
+				log.error("{}", e);
+			}
+
+			if (result != null)
+			{
+				log.info(result.getDspurl());
+			}
 		}
 	}
 
@@ -60,16 +80,28 @@ public class CookieMappingDemo
 		log.info("begin {}", url);
 		log.debug(adxUser.toString());
 
-		HttpPost post = new HttpPost(url);
-		post.setHeader("Host", host);
-		post.setHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:29.0) Gecko/20100101 Firefox/29.0");
-		post.setHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-		post.setHeader("Accept-Language", "zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3");
-		post.setHeader("Accept-Encoding", "gzip, deflate");
-		post.setHeader("Cookie", cookie);
-		post.setHeader("Connection", "keep-alive");
+		HttpGet get = new HttpGet("http://cms.tanx.com/t.gif?id=34462052");
 
-		CloseableHttpResponse response = httpclient.execute(post);
+		Builder configBuilder = RequestConfig.custom().setRedirectsEnabled(false);
+		if (true) //是否设置超时
+		{
+			int timeout = 10000;
+			configBuilder.setSocketTimeout(timeout).setConnectTimeout(timeout).setConnectionRequestTimeout(timeout);
+		}
+		RequestConfig config = configBuilder.build();
+		get.setConfig(config);
+
+		get.setHeader("Host", host);
+		get.setHeader("Connection", "keep-alive");
+		get.setHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+		get.setHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.89 Safari/537.36");
+		get.setHeader("Accept-Encoding", "gzip, deflate, sdch");
+		get.setHeader("Accept-Language", "zh-CN,zh;q=0.8");
+		get.setHeader("Cookie", cookie);
+
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		CloseableHttpResponse response = httpclient.execute(get);
+		httpclient.close();
 
 		Header[] headers2 = response.getHeaders("Location");
 		for (Header header : headers2)
