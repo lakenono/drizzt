1a10af8c2d89cc07e886501cd477bdc337a69d88
shilei9213 <shilei9213@126.com>
2015-09-19 15:19:51 +0800

    生产者消费者模式线程池

---
+++ src/main/java/drizzt/util/thread/BlockingTaskQueueThreadPool.java
@@ -0,0 +1,57 @@
+package drizzt.util.thread;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class BlockingTaskQueueThreadPool {
+	private BlockingQueue<Runnable> taskQueue;
+	private ThreadPoolExecutor threadPool;
+
+	public BlockingTaskQueueThreadPool(int threadPoolSize, int taskQueueSize) {
+		taskQueue = new LinkedBlockingQueue<Runnable>(taskQueueSize);
+		threadPool = new ThreadPoolExecutor(threadPoolSize, threadPoolSize, 0L, TimeUnit.MILLISECONDS, taskQueue, new RejectedExecutionHandler() {
+
+			@Override
+			public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+				if (!executor.isShutdown()) {
+					try {
+						executor.getQueue().put(r);
+					} catch (InterruptedException e) {
+					}
+				}
+			}
+
+		});
+	}
+
+	/**
+	 * 提交任务，会阻塞
+	 * 
+	 * @param task
+	 */
+	public void submitTask(Runnable task) {
+		threadPool.execute(task);
+	}
+
+	/**
+	 * 等待结束
+	 */
+	public void waitTermianl() {
+		threadPool.shutdown();
+
+		try {
+			while (!threadPool.isTerminated()) {
+				Thread.sleep(1000);
+			}
+		} catch (InterruptedException e) {
+		}
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
