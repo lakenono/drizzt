6648a7ccdf24e29167bf1fa37c3a8be74664abb6
shilei9213 <shilei9213@126.com>
2015-09-19 15:19:09 +0800

    配置类增加默认返回值

--- src/main/java/drizzt/conf/Configuration.java
+++ src/main/java/drizzt/conf/Configuration.java
@@ -2,25 +2,30 @@ package drizzt.conf;
 
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.XMLConfiguration;
+import org.apache.commons.lang3.StringUtils;
 
 public class Configuration {
-	
-	private static  org.apache.commons.configuration.Configuration config;
-	
+
+	private static org.apache.commons.configuration.Configuration config;
+
 	static {
 		try {
 			config = new XMLConfiguration("config.xml");
 		} catch (ConfigurationException e) {
-			throw new RuntimeException("config.xml read error : ",e);
+			throw new RuntimeException("config.xml read error : ", e);
 		}
 	}
-	
-	
-	public static String getString(String key){
+
+	public static String getString(String key) {
 		return config.getString(key);
 	}
-	
+
+	public static String getString(String key, String defaultValue) {
+		String value = config.getString(key);
+		return StringUtils.isNotBlank(value) ? value : defaultValue;
+	}
+
 	public static void main(String[] args) {
-		System.out.println(Configuration.getString("datasource"));
+		System.out.println(Configuration.getString("doddata-test.cleandb"));
 	}
 }
